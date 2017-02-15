46f0907f42815ea93c49d1fb3bce6b1dec81e5e0
Michael Reuter <mareuternh@gmail.com>
2017-02-14 21:36:39 -0700

    Adding tab bar controller.

---
+++ LunarClubTools/LunarClubToolsTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  LunarClubToolsTabBarController.swift
+//  LunarClubTools
+//
+//  Created by Michael Reuter on 2/14/17.
+//  Copyright © 2017 Type II Software. All rights reserved.
+//
+
+import UIKit
+
+class LunarClubToolsTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
