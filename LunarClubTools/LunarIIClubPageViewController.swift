d4d93e52ad19fac634875f03609d056a184643ad
Michael Reuter <mareuternh@gmail.com>
2017-05-30 21:07:22 -0700

    Laying out storyboard.

---
+++ LunarClubTools/LunarIIClubPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LunarIIClubPageViewController.swift
+//  LunarClubTools
+//
+//  Created by Michael Reuter on 5/30/17.
+//  Copyright © 2017 Type II Software. All rights reserved.
+//
+
+import UIKit
+
+class LunarIIClubPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
