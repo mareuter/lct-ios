7001b0489fcfb8f0a035dbab812ead0b1b7897c0
Michael Reuter <mareuternh@gmail.com>
2017-06-02 21:49:03 -0700

    Updating MoonInfo struct with new information.

--- LunarClubTools/MoonInfo.swift
+++ LunarClubTools/MoonInfo.swift
@@ -18,7 +18,17 @@ struct MoonInfo
     let altitude: Double
     let azimuth: Double
     let phaseName: String
+    let rightAscension: Double
+    let declination: Double
+    let elongation: Double
+    let angularSize: Double
+    let magnitude: Double
+    let earthDistance: Double
+    let subSolarLatitude: Double
     let nextFourPhases: Array<(String, Array<Int>)>
+    var riseTime: Any?
+    var setTime: Any?
+    var transitTime: Any?
     
     init?(jsonFile: Data) {
         //print(String(data: jsonFile, encoding: String.Encoding.utf8) ?? "Cannot print data")
@@ -37,7 +47,15 @@ struct MoonInfo
             let altitude = json!["altitude"] as? Double,
             let azimuth = json!["azimuth"] as? Double,
             let phaseName = json!["phase"] as? String,
-            let nextFourPhasesJSON = json!["next_four_phases"] as? [String: [String: Any]]
+            let rightAscension = json!["ra"] as? Double,
+            let declination = json!["declination"] as? Double,
+            let elongation = json!["elongation"] as? Double,
+            let angularSize = json!["angular_size"] as? Double,
+            let magnitude = json!["magnitude"] as? Double,
+            let earthDistance = json!["earth_distance"] as? Double,
+            let subSolarLatitude = json!["subsolar_lat"] as? Double,
+            let nextFourPhasesJSON = json!["next_four_phases"] as? [String: [String: Any]],
+            let riseSetTimesJSON = json!["rise_set_times"] as? [String: [String: Any]]
         else {
             return nil
         }
@@ -49,6 +67,24 @@ struct MoonInfo
             }
         }
         
+        for timeId in Array(riseSetTimesJSON.keys) {
+            if let timeInfo = riseSetTimesJSON[timeId] {
+                let name = timeInfo["time"] as? String ?? ""
+                let datetime = timeInfo["datetime"]
+                switch name {
+                case "rise":
+                    self.riseTime = datetime
+                case "transit":
+                    self.transitTime = datetime
+                case "set":
+                    self.setTime = datetime
+                default:
+                    break
+                }
+            }
+        }
+        
+        
         self.age = age
         self.colongitude = colongitude
         self.fractionalPhase = fractionalPhase
@@ -57,6 +93,13 @@ struct MoonInfo
         self.altitude = altitude
         self.azimuth = azimuth
         self.phaseName = phaseName
+        self.rightAscension = rightAscension
+        self.declination = declination
+        self.elongation = elongation
+        self.angularSize = angularSize
+        self.magnitude = magnitude
+        self.earthDistance = earthDistance
+        self.subSolarLatitude = subSolarLatitude
         self.nextFourPhases = nextFourPhases
     }
     
